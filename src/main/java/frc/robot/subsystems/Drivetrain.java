46339d46449d348e3d4e6d248764014436c83a87
rorylimligon-glitch <rorylimligon@gmail.com>
2025-11-18 20:12:56 -0500

    saved changes

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -23,110 +23,112 @@ import frc.robot.Constants.MotorIdConstants;
 import frc.robot.Constants.MotorConstants;
 
 public class Drivetrain extends SubsystemBase {
-    private SparkMax rightFrontMotorController = new SparkMax(MotorIdConstants.RIGHT_FRONT_ID,
-            SparkLowLevel.MotorType.kBrushless);
-    private SparkMax leftFrontMotorController = new SparkMax(MotorIdConstants.LEFT_FRONT_ID,
-            SparkLowLevel.MotorType.kBrushless);
-    private SparkMax rightBackMotorController = new SparkMax(MotorIdConstants.RIGHT_BACK_ID,
-            SparkLowLevel.MotorType.kBrushless);
-    private SparkMax leftBackMotorController = new SparkMax(MotorIdConstants.LEFT_BACK_ID,
-            SparkLowLevel.MotorType.kBrushless);
+        private SparkMax rightFrontMotorController = new SparkMax(MotorIdConstants.RIGHT_FRONT_ID,
+                        SparkLowLevel.MotorType.kBrushless);
+        private SparkMax leftFrontMotorController = new SparkMax(MotorIdConstants.LEFT_FRONT_ID,
+                        SparkLowLevel.MotorType.kBrushless);
+        private SparkMax rightBackMotorController = new SparkMax(MotorIdConstants.RIGHT_BACK_ID,
+                        SparkLowLevel.MotorType.kBrushless);
+        private SparkMax leftBackMotorController = new SparkMax(MotorIdConstants.LEFT_BACK_ID,
+                        SparkLowLevel.MotorType.kBrushless);
 
-    private final SparkClosedLoopController motorRightClosedLoopController;
-    private final SparkClosedLoopController motorLeftClosedLoopController;
+        private final SparkClosedLoopController motorRightClosedLoopController;
+        private final SparkClosedLoopController motorLeftClosedLoopController;
 
-    private final Distance ENCODER_POSITION_FACTOR = Inches.of(8 * Math.PI * (10. / 52.) * (30. / 68.));
-    private final Distance ENCODER_VELOCITY_FACTOR = Inches.of((8 * Math.PI * (10. / 52.) * (30. / 68.)) / 60.0); // inches
-                                                                                                                  // per
-                                                                                                                  // rotation
+        private final Distance ENCODER_POSITION_FACTOR = Inches.of(8 * Math.PI * (10. / 52.) * (30. / 68.));
+        private final Distance ENCODER_VELOCITY_FACTOR = Inches.of((8 * Math.PI * (10. / 52.) * (30. / 68.)) / 60.0); // inches
+                                                                                                                      // per
+                                                                                                                      // rotation
 
-    private final double drivetrainP = 0.08;
+        private final double drivetrainP = 0.08;
 
-    private final double MAX_SPEED_METERS_PER_SECOND = 4.0;
+        private final double MAX_SPEED_METERS_PER_SECOND = 4.0;
 
-    public Drivetrain() {
-        SparkMaxConfig rightFrontConfig = new SparkMaxConfig();
-        SparkMaxConfig leftFrontConfig = new SparkMaxConfig();
-        SparkMaxConfig rightBackConfig = new SparkMaxConfig();
-        SparkMaxConfig leftBackConfig = new SparkMaxConfig();
+        public Drivetrain() {
+                SparkMaxConfig rightFrontConfig = new SparkMaxConfig();
+                SparkMaxConfig leftFrontConfig = new SparkMaxConfig();
+                SparkMaxConfig rightBackConfig = new SparkMaxConfig();
+                SparkMaxConfig leftBackConfig = new SparkMaxConfig();
 
-        rightFrontConfig.idleMode(IdleMode.kBrake);
-        leftFrontConfig.idleMode(IdleMode.kBrake);
-        rightBackConfig.idleMode(IdleMode.kBrake);
-        leftBackConfig.idleMode(IdleMode.kBrake);
+                rightFrontConfig.idleMode(IdleMode.kBrake);
+                leftFrontConfig.idleMode(IdleMode.kBrake);
+                rightBackConfig.idleMode(IdleMode.kBrake);
+                leftBackConfig.idleMode(IdleMode.kBrake);
 
-        rightFrontConfig.inverted(false);
-        rightBackConfig.inverted(false);
-        leftFrontConfig.inverted(true);
-        leftBackConfig.inverted(true);
+                rightFrontConfig.inverted(false);
+                rightBackConfig.inverted(false);
+                leftFrontConfig.inverted(true);
+                leftBackConfig.inverted(true);
 
-        rightBackConfig.follow(rightFrontMotorController, false);
-        leftBackConfig.follow(leftFrontMotorController, false);
+                rightBackConfig.follow(rightFrontMotorController, false);
+                leftBackConfig.follow(leftFrontMotorController, false);
 
-        rightFrontConfig.smartCurrentLimit((int) MotorConstants.NEO_CURRENT_LIMIT.in(Amps));
-        leftFrontConfig.smartCurrentLimit((int) MotorConstants.NEO_CURRENT_LIMIT.in(Amps));
-        rightBackConfig.smartCurrentLimit((int) MotorConstants.NEO_CURRENT_LIMIT.in(Amps));
-        leftBackConfig.smartCurrentLimit((int) MotorConstants.NEO_CURRENT_LIMIT.in(Amps));
+                rightFrontConfig.smartCurrentLimit((int) MotorConstants.NEO_CURRENT_LIMIT.in(Amps));
+                leftFrontConfig.smartCurrentLimit((int) MotorConstants.NEO_CURRENT_LIMIT.in(Amps));
+                rightBackConfig.smartCurrentLimit((int) MotorConstants.NEO_CURRENT_LIMIT.in(Amps));
+                leftBackConfig.smartCurrentLimit((int) MotorConstants.NEO_CURRENT_LIMIT.in(Amps));
 
-        rightFrontConfig.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR.in(Meters));
-        leftFrontConfig.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR.in(Meters));
-        rightBackConfig.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR.in(Meters));
-        leftBackConfig.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR.in(Meters));
+                rightFrontConfig.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR.in(Meters));
+                leftFrontConfig.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR.in(Meters));
+                rightBackConfig.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR.in(Meters));
+                leftBackConfig.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR.in(Meters));
 
-        rightFrontConfig.encoder.velocityConversionFactor(ENCODER_VELOCITY_FACTOR.in(Meters));
-        leftFrontConfig.encoder.velocityConversionFactor(ENCODER_VELOCITY_FACTOR.in(Meters));
-        rightBackConfig.encoder.velocityConversionFactor(ENCODER_VELOCITY_FACTOR.in(Meters));
-        leftBackConfig.encoder.velocityConversionFactor(ENCODER_VELOCITY_FACTOR.in(Meters));
+                rightFrontConfig.encoder.velocityConversionFactor(ENCODER_VELOCITY_FACTOR.in(Meters));
+                leftFrontConfig.encoder.velocityConversionFactor(ENCODER_VELOCITY_FACTOR.in(Meters));
+                rightBackConfig.encoder.velocityConversionFactor(ENCODER_VELOCITY_FACTOR.in(Meters));
+                leftBackConfig.encoder.velocityConversionFactor(ENCODER_VELOCITY_FACTOR.in(Meters));
 
-        rightFrontConfig.closedLoop.p(drivetrainP);
-        leftFrontConfig.closedLoop.p(drivetrainP);
-        rightBackConfig.closedLoop.p(drivetrainP);
-        leftBackConfig.closedLoop.p(drivetrainP);
+                rightFrontConfig.closedLoop.p(drivetrainP);
+                leftFrontConfig.closedLoop.p(drivetrainP);
+                rightBackConfig.closedLoop.p(drivetrainP);
+                leftBackConfig.closedLoop.p(drivetrainP);
 
-        rightFrontMotorController.configure(
-                rightFrontConfig,
-                SparkMax.ResetMode.kResetSafeParameters,
-                SparkMax.PersistMode.kPersistParameters);
+                rightFrontMotorController.configure(
+                                rightFrontConfig,
+                                SparkMax.ResetMode.kResetSafeParameters,
+                                SparkMax.PersistMode.kPersistParameters);
 
-        leftFrontMotorController.configure(
-                leftFrontConfig,
-                SparkMax.ResetMode.kResetSafeParameters,
-                SparkMax.PersistMode.kPersistParameters);
+                leftFrontMotorController.configure(
+                                leftFrontConfig,
+                                SparkMax.ResetMode.kResetSafeParameters,
+                                SparkMax.PersistMode.kPersistParameters);
 
-        rightBackMotorController.configure(
-                rightBackConfig,
-                SparkMax.ResetMode.kResetSafeParameters,
-                SparkMax.PersistMode.kPersistParameters);
+                rightBackMotorController.configure(
+                                rightBackConfig,
+                                SparkMax.ResetMode.kResetSafeParameters,
+                                SparkMax.PersistMode.kPersistParameters);
 
-        leftBackMotorController.configure(
-                leftBackConfig,
-                SparkMax.ResetMode.kResetSafeParameters,
-                SparkMax.PersistMode.kPersistParameters);
+                leftBackMotorController.configure(
+                                leftBackConfig,
+                                SparkMax.ResetMode.kResetSafeParameters,
+                                SparkMax.PersistMode.kPersistParameters);
 
-        motorRightClosedLoopController = rightFrontMotorController.getClosedLoopController();
-        motorLeftClosedLoopController = leftFrontMotorController.getClosedLoopController();
+                motorRightClosedLoopController = rightFrontMotorController.getClosedLoopController();
+                motorLeftClosedLoopController = leftFrontMotorController.getClosedLoopController();
 
-    }
+        }
 
-    public void setRightSpeed(LinearVelocity speed) {
-        motorRightClosedLoopController.setReference(speed.in(MetersPerSecond), ControlType.kVelocity);
-    }
+        public void setRightSpeed(LinearVelocity speed) {
+                motorRightClosedLoopController.setReference(speed.in(MetersPerSecond), ControlType.kVelocity);
+        }
 
-    public void setLeftSpeed(LinearVelocity speed) {
-        motorLeftClosedLoopController.setReference(speed.in(MetersPerSecond), ControlType.kVelocity);
-    }
+        public void setLeftSpeed(LinearVelocity speed) {
+                motorLeftClosedLoopController.setReference(speed.in(MetersPerSecond), ControlType.kVelocity);
+        }
 
-    public Command tankDrive(DoubleSupplier rightSpeed, DoubleSupplier leftSpeed,
-            BooleanSupplier runWhenRightTriggerDown) {
-        return this.run(() -> {
-            if (runWhenRightTriggerDown.getAsBoolean()) {
-                setLeftSpeed((MetersPerSecond.of(leftSpeed.getAsDouble() * MAX_SPEED_METERS_PER_SECOND)));
-                setRightSpeed((MetersPerSecond.of(rightSpeed.getAsDouble() * MAX_SPEED_METERS_PER_SECOND)));
-            } else {
-                setLeftSpeed((MetersPerSecond.of(0)));
-                setRightSpeed((MetersPerSecond.of(0)));
-            }
-        });
+        public Command tankDrive(DoubleSupplier rightSpeed, DoubleSupplier leftSpeed,
+                        BooleanSupplier runWhenRightTriggerDown) {
+                return this.run(() -> {
+                        if (runWhenRightTriggerDown.getAsBoolean()) {
+                                setLeftSpeed((MetersPerSecond
+                                                .of(leftSpeed.getAsDouble() * MAX_SPEED_METERS_PER_SECOND)));
+                                setRightSpeed((MetersPerSecond
+                                                .of(rightSpeed.getAsDouble() * MAX_SPEED_METERS_PER_SECOND)));
+                        } else {
+                                setLeftSpeed((MetersPerSecond.of(0)));
+                                setRightSpeed((MetersPerSecond.of(0)));
+                        }
+                });
 
-    }
+        }
 }
